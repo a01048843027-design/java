49d939aad08bac6ad73ff18a77cb2864f5e1691b
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:45:57 +0900

    Update Practice_07.java

--- 0918/report/Practice_07.java
+++ 0918/report/Practice_07.java
@@ -4,33 +4,24 @@
 public class Practice_07 {
 
     public static void main(String[] args) {
-        // 10개의 정수를 저장할 배열 생성
         int[] intArray = new int[10];
         
-        // 평균 계산을 위해 합계를 저장할 변수. 정확한 계산을 위해 double 타입 사용
         double sum = 0.0;
 
-        // 배열 길이만큼 반복하여 랜덤 정수를 생성하고 저장
         for (int i = 0; i < intArray.length; i++) {
-            // 11~19 범위의 랜덤 정수 생성
-            // Math.random() : 0.0 이상 1.0 미만의 double 값 반환
-            // * 9           : 0.0 이상 9.0 미만
-            // (int)         : 0 ~ 8 사이의 정수
-            // + 11          : 11 ~ 19 사이의 정수
+
             int randomNum = (int) (Math.random() * 9) + 11;
             
             intArray[i] = randomNum; // 배열에 랜덤 정수 저장
             sum += intArray[i];      // 생성된 정수를 sum에 더함
         }
 
-        // 배열에 저장된 정수들 출력
         System.out.print("랜덤한 정수들 : ");
         for (int i = 0; i < intArray.length; i++) {
             System.out.print(intArray[i] + " ");
         }
         System.out.println();
 
-        // 평균을 계산하고 출력
         double average = sum / intArray.length;
         System.out.println("평균은 " + average);
     }
