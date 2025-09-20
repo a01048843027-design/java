30bb3a6b2be1501e5e2155e05102cd515540fac1
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:46:22 +0900

    Update Practice_06.java

--- 0918/report/Practice_06.java
+++ 0918/report/Practice_06.java
@@ -17,19 +17,17 @@ public class Practice_06 {
 
         System.out.print("자리수의 합이 9인 것은 ");
 
-        // 배열의 각 원소를 순회합니다.
         for (int i = 0; i < intArray.length; i++) {
-            int number = intArray[i]; // 현재 검사할 숫자
-            int temp = number;        // 계산을 위해 숫자를 임시 변수에 복사
-            int sum = 0;              // 자리수의 합을 저장할 변수
+            int number = intArray[i]; 
+            int temp = number;        
+            int sum = 0;            
 
             // while문을 이용해 각 자리수를 더합니다.
             while (temp > 0) {
-                sum += temp % 10; // 숫자의 마지막 자리수를 sum에 더함
-                temp /= 10;       // 숫자의 마지막 자리수를 제거
+                sum += temp % 10;
+                temp /= 10;    
             }
 
-            // 자리수의 합이 9와 같으면 원래 숫자를 출력합니다.
             if (sum == 9) {
                 System.out.print(number + " ");
             }
