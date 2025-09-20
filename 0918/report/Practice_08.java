099be854a0993adb8786db2d89d0d320af891b75
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:52:05 +0900

    Update Practice_08.java

--- 0918/report/Practice_08.java
+++ 0918/report/Practice_08.java
@@ -1,5 +1,11 @@
 // 8. 몇 개의 정수를 입력할 것인지 정수 개수를 입력받고, 그 만큼의 임의의 정수를 입력받아 배열에 저장한다.
 //    단, 정수가 1~100 범위의 정수가 아니면 다시 입력받도록 하고, 배열에 저장된 정수들과 평균을 출력하라.
+/*
+    정수 몇 개>>7
+    67 69 31 48 27 20 92 55 50 47 93 75
+    67 69 31 48 27 20 92
+    평균은 54.857142857142854
+*/
 
 import java.util.Scanner;
 
@@ -17,14 +23,11 @@ public class Practice_08 {
             int num = scanner.nextInt();
 
             if (num >= 1 && num <= 100) {
-                intArray[i] = num; // 유효하면 배열에 저장
+                intArray[i] = num;
+                sum += num;
             } else {
                 i--;
             }
-        }
-
-        for (int i = 0; i < intArray.length; i++) {
-            sum += intArray[i];
         }
 
         for (int i = 0; i < intArray.length; i++) {
