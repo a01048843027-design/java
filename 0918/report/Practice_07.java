d3bb24112badba32cf8e688c7447c7af5407466d
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:51:36 +0900

    Update Practice_07.java

--- 0918/report/Practice_07.java
+++ 0918/report/Practice_07.java
@@ -1,19 +1,20 @@
 // 7. 정수를 10개 저장하는 배열을 만들고, 11~19 범위의 정수를 랜덤하게 생성하여 배열에 저장하라.
 //    그리고 배열에 들어 있는 정수들과 평균을 출력하라.
+/*
+    랜덤한 정수들...16 13 17 19 12 16 14 17 16 11
+    평균은 15.1
+*/
 
 public class Practice_07 {
 
     public static void main(String[] args) {
         int[] intArray = new int[10];
-        
         double sum = 0.0;
 
         for (int i = 0; i < intArray.length; i++) {
-
             int randomNum = (int) (Math.random() * 9) + 11;
-            
-            intArray[i] = randomNum; // 배열에 랜덤 정수 저장
-            sum += intArray[i];      // 생성된 정수를 sum에 더함
+            intArray[i] = randomNum;
+            sum += intArray[i];
         }
 
         System.out.print("랜덤한 정수들 : ");
