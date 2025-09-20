a1f4a1d29f40bb691cd33d18573e2bf710a92dea
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:47:12 +0900

    Update Practice_04.java

--- 0918/report/Practice_04.java
+++ 0918/report/Practice_04.java
@@ -4,17 +4,12 @@
 public class Practice_04 {
 
     public static void main(String[] args) {
-        // 문제에 주어진 2차원 배열 초기화
         int n[][] = {{1, 2, 3}, {1, 2}, {1}, {1, 2, 3, 4}};
 
-        // n.length는 배열의 행(row) 개수만큼 반복합니다. (총 4번)
         for (int i = 0; i < n.length; i++) {
-            // n[i].length는 현재 행의 열(column) 개수만큼 반복합니다.
             for (int j = 0; j < n[i].length; j++) {
-                // 배열의 원소를 출력하고 한 칸 띄웁니다.
                 System.out.print(n[i][j] + " ");
             }
-            // 한 행의 출력이 끝나면 줄을 바꿉니다.
             System.out.println();
         }
     }
