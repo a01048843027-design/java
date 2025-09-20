13e5c844d4fa4f072cbd55cb6ce447004edcd071
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:53:02 +0900

    Update Practice_10.java

--- 0918/report/Practice_10.java
+++ 0918/report/Practice_10.java
@@ -1,14 +1,27 @@
-// 10. (문제의 설명이 불분명하여 실행 사례를 기반으로 재구성)
-// 4x4 크기의 2차원 정수 배열을 0~255 범위의 랜덤 정수로 초기화하고 출력합니다.
-// 그 다음, 사용자로부터 정수 하나를 입력받습니다.
-// 배열의 각 원소가 사용자가 입력한 값보다 크면 255로, 그렇지 않으면 0으로 변경한 후
-// 수정된 배열을 다시 출력하는 프로그램을 작성하라.
+// 10. (실행 사례 기반) 4x4 배열을 0~255 범위의 랜덤 정수로 초기화 후 출력하고,
+//     사용자에게 정수를 입력받아 배열의 값이 입력값보다 크면 255, 작거나 같으면 0으로 변경하여 다시 출력하라.
+/*
+    4x4 배열에 랜덤한 값을 저장한 후 출력합니다.
+    239 0   184 166
+    192 165 129 194
+    59  110 255 168
+    97  7   157 65
+
+    임의의 값>>100
+
+    0   0   255 255
+    255 255 255 255
+    0   255 255 255
+    0   0   255 0
+*/
+
 
 import java.util.Scanner;
 
 public class Practice_10 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int[][] intArray = new int[4][4];
 
         for (int i = 0; i < intArray.length; i++) {
@@ -24,12 +37,11 @@ public class Practice_10 {
             }
             System.out.println();
         }
-        
-        System.out.println(); 
+        System.out.println();
 
-        Scanner scanner = new Scanner(System.in);
         System.out.print("임의의 값>>");
         int threshold = scanner.nextInt();
+        System.out.println();
 
         for (int i = 0; i < intArray.length; i++) {
             for (int j = 0; j < intArray[i].length; j++) {
@@ -40,9 +52,6 @@ public class Practice_10 {
                 }
             }
         }
-
-        System.out.println(); 
-
 
         for (int i = 0; i < intArray.length; i++) {
             for (int j = 0; j < intArray[i].length; j++) {
