f0bc91116731e37e2b9caf82ccef493d22373255
a01048843027-design <a01048843027@gmail.com>
2025-09-20 10:37:58 +0900

    Create Practice_03.java

---
+++ 0918/report/Practice_03.java
@@ -0,0 +1,29 @@
+// 3. Scanner를 이용하여 양의 정수를 입력받고 다음과 같이 *를 출력하는 프로그램을 작성하라.
+// 양의 정수가 입력될 때까지 계속 입력을 받도록 하라. 다음 실행 사례는 5를 입력받은 경우이다.
+
+import java.util.Scanner;
+
+public class Practice_03 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        // 양의 정수가 입력될 때까지 입력을 반복합니다.
+        do {
+            System.out.print("양의 정수 입력>>");
+            n = scanner.nextInt();
+        } while (n <= 0);
+
+        // 입력된 숫자 n부터 1까지 줄어드는 역순으로 반복합니다.
+        for (int i = n; i > 0; i--) {
+            // 현재 행(i)의 숫자만큼 '*'를 출력합니다.
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
