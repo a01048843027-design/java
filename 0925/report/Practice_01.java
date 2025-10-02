2b711c1c96b709b8f025da6782c1e26726681f7a
a01048843027-design <a01048843027@gmail.com>
2025-10-02 19:17:46 +0900

    Create Practice_01.java

---
+++ 0925/report/Practice_01.java
@@ -0,0 +1,34 @@
+// 1. 자바 클래스를 만들어보자. 다음 main() 메소드를 실행하였을 때 예시와 같이 출력되도록
+//    TV 클래스를 작성하라.
+/*
+    public static void main(String[] args) {
+        TV tv = new TV("Samsung", 50, 300); // 300만원짜리 Samsung에서 만든 50인치 TV
+        tv.show();
+    }
+*/
+/*
+    Samsung에서 만든 300만원짜리 50인치 TV
+*/
+
+class TV {
+    private String manufacturer;
+    private int size;
+    private int price;
+
+    public TV(String manufacturer, int size, int price) {
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void show() {
+        System.out.println(manufacturer + "에서 만든 " + price + "만원짜리 " + size + "인치 TV");
+    }
+}
+
+public class Practice_ch04_01 {
+    public static void main(String[] args) {
+        TV tv = new TV("Samsung", 50, 300);
+        tv.show();
+    }
+}
